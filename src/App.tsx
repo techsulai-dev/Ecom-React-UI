ce8ade8d11aa6bfd1b8913298970649e734afd1d
techsulai-dev <techsulai786@gmail.com>
2025-09-06 14:00:29 +0530

    r

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './Pages/Login';
 import Dashboard from './Pages/Dashboard';
 import './App.css';
+import { HashRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename={import.meta.env.BASE_URL}>
+    <HashRouter >
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
