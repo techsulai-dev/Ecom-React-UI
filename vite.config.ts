f2de77a6b0eabb5f1458a23b4d900f1f97162e71
techsulai-dev <techsulai786@gmail.com>
2025-09-06 11:34:18 +0530

    base slash

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ecom-quickcommerce/', // 👈 replace with your repo name
+  base: '/', // 👈 replace with your repo name
   plugins: [react()],
   test: {
     globals: true,         // allows using expect/describe without import
