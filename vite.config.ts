817f7f4ac320b1bcc117ae93ccd5ef9c87cca930
techsulai-dev <techsulai786@gmail.com>
2025-09-06 14:51:53 +0530

    nn

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default ({ mode }: { mode: string }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_')
-
-  const baseMap: Record<string, string> = {
-    local: '/',
-    dev: '/',
-    qa: '/',
-    uat: '/',
-    prod: '/Ecom-React-UI/',  // GitHub Pages requires repo name as base
-  }
-  return defineConfig({
-    base: baseMap[mode],
-    // base: '/Ecom-React-UI/',
+export default defineConfig(({ mode }) => {
+  return {
     plugins: [react()],
-    define: {
-      __APP_ENV__: env.VITE_APP_ENV
-    }
-  })
-}
+    base: mode === 'production' ? '/Ecom-React-UI/' : '/', // 👈 switch base by mode
+  };
+});
